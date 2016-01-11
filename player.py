a4770c0dfc336e8b6963150d0b93de5005287ef6
masonasons <masonarm@gmail.com>
2016-01-11 02:29:05 -0600

    Rewrites player to use the CMD object, thanks Chris Toth.

--- player.py
+++ player.py
@@ -1,6 +1,39 @@
-import sound_lib
 import audio_services
+import cmd
+
+import sound_lib
 import audio_player
+player=audio_player.URLStream()
+class commandline(cmd.Cmd):
+	def help_vd(self):
+		print "Turns down the volume of the audio."
+	def do_vd(self, line):
+		if player.stream.volume<=0.1:
+			print "Volume is minimum"
+		else:
+			player.stream.volume-=0.05
+		print "Volume is now "+str(round(player.stream.volume,2))
+	def help_vu(self):
+		print "Turns up the volume of the audio."
+	def do_vu(self, line):
+		if player.stream.volume>=1.0:
+			print "Volume is maximum"
+		else:
+			player.stream.volume+=0.05
+		print "Volume is now "+str(round(player.stream.volume,2))
+	def help_pause(self):
+		print "Pauses the audio."
+	def do_pause(self, line):
+		if player.stream.is_paused==False:
+			player.stream.pause()
+			print "Paused"
+		else:
+			player.stream.play()
+			print "Playing"
+
+	def do_EOF(self, line):
+		return True
+
 cont=0
 player=audio_player.URLStream()
 text=raw_input("Enter the address.")
@@ -10,28 +43,6 @@ try:
 except:
 	print "Error. Unable to play."
 	cont=1
-while True:
-	if cont==1:
-		break
-	stuff=raw_input("Do what")
-	if stuff=="pause":
-		if player.stream.is_paused==False:
-			player.stream.pause()
-			print "Paused"
-		else:
-			player.stream.play()
-			print "Playing"
-	if stuff=="vu":
-		if player.stream.volume>=1.0:
-			print "Volume is maximum"
-		else:
-			player.stream.volume+=0.05
-		print "Volume is now "+str(round(player.stream.volume,2))
-	if stuff=="vd":
-		if player.stream.volume<=0.1:
-			print "Volume is minimum"
-		else:
-			player.stream.volume-=0.05
-		print "Volume is now "+str(round(player.stream.volume,2))
-	if stuff=="quit":
-		break
+
+if cont==0:
+	commandline().cmdloop("Audio is playing. Type help for help.")
